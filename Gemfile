4a6ef3edae0aa15272eb17334428ced12af8a270
Edmond <edmond.b@hotmail.co.uk>
2021-03-15 11:02:21 +0000

    Framework set up and first passing test added

---
+++ Gemfile
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+gem 'sinatra'
+gem 'capybara'
+gem 'selenium-webdriver'
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+ruby '2.6.6'
+
+group :development, :test do
+  gem "rspec"
+  gem 'rubocop', '0.79.0'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,22 @@
+ENV['RACK ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = Bookmark
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+end

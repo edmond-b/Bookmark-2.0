0d230cf34cbd3303a584b117554119dc99cfd10b
Edmond <edmond.b@hotmail.co.uk>
2021-03-22 17:01:51 +0000

    Authenticate method now checks if email is valid

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -10,10 +10,10 @@ feature 'Authentication' do
 
   scenario 'a user sees an error if they get their email wrong' do
     User.create(email: 'test@example.com', password: 'password123')
-    visit '/sessions/new'
+    visit '/session/new'
     fill_in(:email, with: 'nottherightemail@me.com')
     fill_in(:password, with: 'password123')
-    click_button('Sign in')
+    click_button('Sign In')
     expect(page).not_to have_content 'Welcome, test@example.com'
     expect(page).to have_content 'Please check your email or password.'
   end
